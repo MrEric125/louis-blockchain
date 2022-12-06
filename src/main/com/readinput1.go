365782628e90687b1dd92c69214be27389c2ab52
liujun <liujun@baijiayun.com>
2022-12-06 21:03:40 +0800

    add log

--- src/main/com/readinput1.go
+++ src/main/com/readinput1.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -23,11 +25,38 @@ func main() {
 	// fmt.Scanf(input, format, &f, &i, &s)
 	// fmt.Println("from the string we read:", f, i, s)
 
-	inputReader = bufio.NewReader(os.Stdin)
-	fmt.Println("please enter some input:")
-	input, err = inputReader.ReadString('\n')
-	if err == nil {
-		fmt.Printf("the input was :%s\n\":", input)
+	// inputReader = bufio.NewReader(os.Stdin)
+	// fmt.Println("please enter some input:")
+	// input, err = inputReader.ReadString('\n')
+	// if err == nil {
+	// 	fmt.Printf("the input was :%s\n\":", input)
+	// }
+	fmt.Print("place input")
+	flag.Parse()
+	if flag.NArg() == 0 {
+		cat(bufio.NewReader(os.Stdin))
 	}
+	for i := 0; i < flag.NArg(); i++ {
+		f, err := os.Open(flag.Arg(i))
+		if err != nil {
+
+		}
+		cat(bufio.NewReader(f))
+		f.Close()
+
+	}
+
+}
+
+func cat(r *bufio.Reader) {
+	for {
+		buf, err := r.ReadBytes('\n')
+		fmt.Fprintf(os.Stdout, "%s", buf)
+		if err == io.EOF {
+			break
+		}
+		fmt.Print("input end")
+	}
+	return
 
 }
